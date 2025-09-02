3ada34ecf9c76a9a3a63834ca723869a4b502e77
brian <dpi.studio@gmail.com>
2025-09-02 22:21:25 +0800

    refactor(App): 移除不必要的路由連結與腳本
    - 精簡 App.vue 檔案，移除不必要的 RouterLink 和腳本
    - 提升組件的可讀性與維護性

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,3 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <header>
-    <RouterLink to="/">SignIn</RouterLink> |
-    <RouterLink to="/signup">SignUp</RouterLink> |
-    <RouterLink to="/todolist">TodoList</RouterLink>
-  </header>
-
   <RouterView />
 </template>
-
-<style scoped></style>
