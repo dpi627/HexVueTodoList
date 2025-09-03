1eed2e5f8d8b99d7ac6c0291aed62f8994b2d3d5
brian <dpi.studio@gmail.com>
2025-09-03 19:51:22 +0800

    feat(TodoForm): 新增 TodoForm 組件
    - 提供用戶輸入待辦事項的功能
    - 支援按下 Enter 鍵或點擊加號按鈕提交待辦事項
    - 整合 Vue 的 reactivity 以管理待辦事項的狀態

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="inputBox">
+    <input type="text" placeholder="請輸入待辦事項" v-model="newTodo" @keypress.enter="handleSubmit">
+    <a href="#" @click.prevent="handleSubmit">
+      <i class="fa fa-plus"></i>
+    </a>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useTodoStore } from '@/stores/todo'
+
+const newTodo = ref('')
+const todoStore = useTodoStore();
+
+const handleSubmit = () => {
+  todoStore.add(newTodo.value)
+  newTodo.value = ''
+}
+
+</script>
