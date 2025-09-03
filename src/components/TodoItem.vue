1abf89af08ec18c848daff3ff0b69a2d3a729fb6
brian <dpi.studio@gmail.com>
2025-09-03 19:51:12 +0800

    feat(TodoItem): 新增 TodoItem 組件
    - 實作待辦事項的顯示與操作功能
    - 包含勾選狀態與刪除按鈕

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <li>
+    <label class="todoList_label">
+      <input class="todoList_input" type="checkbox" :checked="todo.status" @change="todoStore.toggle(todo.id)">
+      <span>{{ todo.content }}</span>
+    </label>
+    <a href="#" @click.prevent="todoStore.remove(todo.id)">
+      <i class="fa fa-times"></i>
+    </a>
+  </li>
+</template>
+
+<script setup>
+import { useTodoStore } from '@/stores/todo'
+
+const todoStore = useTodoStore();
+
+// 定義 props 類型與預設值
+defineProps({
+  todo: {
+    type: Object,
+    default: () => ({})
+  }
+})
+// defineProps(['todos']) // 可用縮寫(不指定類型)
+</script>
