63a60464e48168a8eedafb8f99b239677f0093d2
brian <dpi.studio@gmail.com>
2025-09-04 22:59:05 +0800

    feat(SignUpView): 更新註冊頁面表單
    - 將表單元素綁定至 Vue 的資料模型
    - 使用 router-link 取代原有的登入連結
    - 增加註冊成功與失敗的提示

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -13,19 +13,52 @@
         <form class="formControls" action="index.html">
           <h2 class="formControls_txt">註冊帳號</h2>
           <label class="formControls_label" for="email">Email</label>
-          <input class="formControls_input" type="text" id="email" name="email" placeholder="請輸入 email" required>
+          <input class="formControls_input" v-model="email" type="text" id="email" name="email" placeholder="請輸入 email"
+            required>
+          <span>此欄位不可留空</span>
           <label class="formControls_label" for="name">您的暱稱</label>
-          <input class="formControls_input" type="text" name="name" id="name" placeholder="請輸入您的暱稱">
+          <input class="formControls_input" v-model="nickname" type="text" name="name" id="name" placeholder="請輸入您的暱稱">
           <label class="formControls_label" for="pwd">密碼</label>
-          <input class="formControls_input" type="password" name="pwd" id="pwd" placeholder="請輸入密碼" required>
+          <input class="formControls_input" v-model="password" type="password" name="pwd" id="pwd" placeholder="請輸入密碼"
+            required>
           <label class="formControls_label" for="pwd">再次輸入密碼</label>
-          <input class="formControls_input" type="password" name="pwd" id="pwd" placeholder="請再次輸入密碼" required>
-          <input class="formControls_btnSubmit" type="button" onclick="javascript:location.href='#todoListPage'"
-            value="註冊帳號">
-          <a class="formControls_btnLink" href="#loginPage">登入</a>
+          <input class="formControls_input" v-model="passwordAgain" type="password" name="pwd" id="pwd2"
+            placeholder="請再次輸入密碼" required>
+          <input class="formControls_btnSubmit" @click="handleSignUp" type="button" value="註冊帳號" />
+          <router-link to="SignIn" class="formControls_btnLink">登入</router-link>
         </form>
       </div>
     </div>
 
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import { signUp } from '@/utils/api'
+
+const email = ref('exm@gmail.com')
+const password = ref('abc123')
+const passwordAgain = ref('abc123')
+const nickname = ref('dpi')
+
+
+const handleSignUp = async () => {
+
+  await signUp(
+    email.value,
+    password.value,
+    nickname.value
+  )
+    .then((response) => {
+      console.log('Sign up successful:', response)
+      alert(`註冊成功: ${response.data.uid}`)
+      // Handle successful sign-up (e.g., redirect to login page)
+    })
+    .catch((error) => {
+      console.error('Sign up error:', error)
+      alert(`註冊失敗: ${error.response.data.message}`)
+      // Handle sign-up error (e.g., show error message)
+    })
+}
+</script>
