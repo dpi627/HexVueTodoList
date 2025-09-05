c70a8c393cf69ffafa57f7e8f4cafea76dac6422
brian <dpi.studio@gmail.com>
2025-09-05 18:40:31 +0800

    feat(dependencies): 新增 vee-validate 和 yup 依賴
    - 更新 package.json 和 package-lock.json，新增 vee-validate 依賴
    - 更新 package.json 和 package-lock.json，新增 yup 依賴

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { configure } from 'vee-validate'
+
+configure({
+  validateOnInput: true, // 輸入時即時驗證
+  validateOnBlur: true, // 失去焦點時驗證
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
