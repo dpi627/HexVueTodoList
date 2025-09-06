9d40f10f0298f71efb72229760358d926dac7e55
brian <brian.li@sgs.com>
2025-09-06 16:53:04 +0800

    style(SignInView, SignUpView): 移除 logo 圍繞的連結
    - 將 logo 圍繞的 <a> 標籤移除，直接顯示圖片
    - 改善頁面結構與可讀性

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -2,9 +2,8 @@
   <div id="signUpPage" class="bg-yellow">
     <div class="conatiner signUpPage vhContainer">
       <div class="side">
-        <a href="#"><img class="logoImg"
-            src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/todolist/logo.png"
-            alt=""></a>
+        <img class="logoImg"
+          src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/todolist/logo.png" alt="">
         <img class="d-m-n"
           src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/todolist/img.png"
           alt="workImg">
