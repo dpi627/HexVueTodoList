09bd3fc448bd986e0eb46ec80b2c1839efa08cce
brian <brian.li@sgs.com>
2025-09-06 10:48:13 +0800

    fix(router): 修正註冊路由的路徑
    - 將註冊路由的路徑從 '/' 修改為 '/signup'
    - 新增根路由的重定向至 '/todolist'

--- src/router/index.js
+++ src/router/index.js
@@ -7,12 +7,17 @@ const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      redirect: '/todolist', // 使用 redirect 重定導向，清楚明白
+      // component: TodoListView, // 也可直接指定元件 (不如 redirect 清楚)
+    },
+    {
       path: '/signin',
       name: 'signin',
       component: SignInView,
     },
     {
-      path: '/',
+      path: '/signup',
       name: 'signup',
       component: SignUpView,
     },
@@ -20,6 +25,7 @@ const router = createRouter({
       path: '/todolist',
       name: 'todolist',
       component: TodoListView,
+      //alias: '/'  // 如不使用前述 redirect，也可用別名
     },
   ],
 })
