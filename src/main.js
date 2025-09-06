ea54db56b7bcae8e381a522ca9fdca2cdf0514f7
brian <brian.li@sgs.com>
2025-09-06 16:56:18 +0800

    style(main.js): 新增註解以改善程式碼可讀性
    - 為 vee-validate 的全域驗證設定新增註解
    - 為 Swal 的全域掛載新增註解

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import router from './router'
 import { configure } from 'vee-validate'
 import Swal from 'sweetalert2'
 
+// vee-validate 的全域驗證設定
 configure({
   validateOnInput: true, // 輸入時即時驗證
   validateOnBlur: true, // 失去焦點時驗證
@@ -18,6 +19,7 @@ configure({
 
 const app = createApp(App)
 
+// 將 Swal 掛載到全域 (方便在任何元件中使用)
 app.config.globalProperties.$swal = Swal
 
 app.use(createPinia())
