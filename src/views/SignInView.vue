058b0e06a7fcd9ea9c16323c1f24f1db6e30a2e2
Brian <dpi.studio@gmail.com>
2025-09-07 15:39:18 +0800

    style(SignInView): 優化登入頁面輸入框的格式
    - 移除多餘的換行，簡化代碼
    - 調整輸入框的格式，使其更一致
    - 移除不必要的預設值設定

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -9,35 +9,14 @@
         <form class="formControls">
           <h2 class="formControls_txt">最實用的線上待辦事項服務</h2>
           <label class="formControls_label" for="email">Email</label>
-          <input
-            v-model="email"
-            @keypress.enter.prevent="submitForm"
-            class="formControls_input"
-            type="text"
-            id="email"
-            name="email"
-            placeholder="請輸入 email"
-            required
-          />
+          <input v-model="email" @keypress.enter.prevent="submitForm" class="formControls_input" type="text" id="email"
+            name="email" placeholder="請輸入 email" required />
           <span v-if="emailError">{{ emailError }}</span>
           <label class="formControls_label" for="pwd">密碼</label>
-          <input
-            v-model="password"
-            @keypress.enter.prevent="submitForm"
-            class="formControls_input"
-            type="password"
-            name="pwd"
-            id="pwd"
-            placeholder="請輸入密碼"
-            required
-          />
+          <input v-model="password" @keypress.enter.prevent="submitForm" class="formControls_input" type="password"
+            name="pwd" id="pwd" placeholder="請輸入密碼" required />
           <span v-if="passwordError">{{ passwordError }}</span>
-          <input
-            @click.prevent="submitForm"
-            class="formControls_btnSubmit"
-            type="button"
-            value="登入"
-          />
+          <input @click.prevent="submitForm" class="formControls_btnSubmit" type="button" value="登入" />
           <router-link to="/signup" class="formControls_btnLink">註冊帳號</router-link>
         </form>
       </div>
@@ -67,9 +46,6 @@ const { handleSubmit } = useForm({ validationSchema: schema })
 // 定義欄位與錯誤訊息
 const { value: email, errorMessage: emailError } = useField('email')
 const { value: password, errorMessage: passwordError } = useField('password')
-
-email.value = 'exm@gmail.com'
-password.value = 'abc123'
 
 // 提交處理方法封裝
 const submitForm = handleSubmit(async (values) => {
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -9,55 +9,23 @@
         <form class="formControls">
           <h2 class="formControls_txt">註冊帳號</h2>
           <label class="formControls_label" for="email">Email</label>
-          <input
-            class="formControls_input"
-            v-model="email"
-            type="text"
-            id="email"
-            name="email"
-            placeholder="請輸入 email"
-            required
-          />
+          <input class="formControls_input" v-model="email" type="text" id="email" name="email" placeholder="請輸入 email"
+            required />
           <span v-if="emailError" class="error">{{ emailError }}</span>
           <!-- 顯示錯誤 -->
           <label class="formControls_label" for="name">您的暱稱</label>
-          <input
-            class="formControls_input"
-            v-model="nickname"
-            type="text"
-            name="name"
-            id="name"
-            placeholder="請輸入您的暱稱"
-          />
+          <input class="formControls_input" v-model="nickname" type="text" name="name" id="name"
+            placeholder="請輸入您的暱稱" />
           <span v-if="nicknameError" class="error">{{ nicknameError }}</span>
           <label class="formControls_label" for="pwd">密碼</label>
-          <input
-            class="formControls_input"
-            v-model="password"
-            type="password"
-            name="pwd"
-            id="pwd"
-            placeholder="請輸入密碼"
-            required
-          />
+          <input class="formControls_input" v-model="password" type="password" name="pwd" id="pwd" placeholder="請輸入密碼"
+            required />
           <span v-if="passwordError" class="error">{{ passwordError }}</span>
           <label class="formControls_label" for="pwd">再次輸入密碼</label>
-          <input
-            class="formControls_input"
-            v-model="passwordConfirm"
-            type="password"
-            name="pwd"
-            id="pwd2"
-            placeholder="請再次輸入密碼"
-            required
-          />
+          <input class="formControls_input" v-model="passwordConfirm" type="password" name="pwd" id="pwd2"
+            placeholder="請再次輸入密碼" required />
           <span v-if="passwordConfirmError" class="error">{{ passwordConfirmError }}</span>
-          <input
-            class="formControls_btnSubmit"
-            type="button"
-            value="註冊帳號"
-            @click="submitForm"
-          />
+          <input class="formControls_btnSubmit" type="button" value="註冊帳號" @click="submitForm" />
           <router-link to="/signin" class="formControls_btnLink">登入</router-link>
         </form>
       </div>
@@ -95,10 +63,6 @@ const { value: email, errorMessage: emailError } = useField('email')
 const { value: nickname, errorMessage: nicknameError } = useField('nickname')
 const { value: password, errorMessage: passwordError } = useField('password')
 const { value: passwordConfirm, errorMessage: passwordConfirmError } = useField('passwordConfirm')
-
-email.value = 'exm@gmail.com'
-nickname.value = 'dpi'
-password.value = passwordConfirm.value = 'abc123'
 
 // 提交處理方法封裝
 const submitForm = handleSubmit(async (values) => {
